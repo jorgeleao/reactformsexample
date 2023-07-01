b3756245483a03c7b8736e94beb7cfa9a1ecffea
Jorge Leao <jorge.ls.leao@gmail.com>
2023-07-01 17:56:17 -0300

    Part of example running O.K.

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,20 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const[field1,setField1] = useState("")
+
+  const handleInput1Change = value => {setField1(value)}
+  const handle_button1_click = (e) => {console.log('=== Clicked button_1: '+field1)}
 
   return (
-    <>
-    </>
+    <div className='root'>
+      <div className='frame'>
+        <input type='text' className='input1_style' value={field1} onChange={e => handleInput1Change(e.target.value)}/>
+        <br/>
+        <br/>
+        <button className='button1_style' onClick={(e) => handle_button1_click(e)}>ENVIAR</button>
+      </div>
+    </div>
   )
 }
 
